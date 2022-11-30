74b2b8eea96c62283aeb8b111a8d70cc1a6500b8
송영배 <ybsong@inzent.com>
2022-11-30 16:22:58 +0900

    apply Checkstyle

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	private static final String constant = "constant";
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
